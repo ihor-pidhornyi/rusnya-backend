ee6c19d976d84eb094b4f86db5d8e80f03c6cffb
ihorpidhornyi <ihorp@rabota.ua>
2022-06-06 16:27:27 +0300

    cors

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const ordersRoutes = require('./routes/orders');
 const app = express();
 
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use('/auth', authRoutes);
